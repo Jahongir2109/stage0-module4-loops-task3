c3dcbc250106e41987f10943fe509a1d4a586e6f
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-05 23:53:23 +0500

    fixed all classes

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -1,6 +1,9 @@
 package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
-    public void printAlphabet() {
+    public  void printAlphabet() {
+        for (int i='a';i<='z';i++){
+            System.out.println((char)i);
+        }
     }
 }
